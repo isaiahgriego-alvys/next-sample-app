148343f48e7cb71d066cc429d3f59729e48bab5f
Isaiah <isaiah.griego@alvys.com>
2025-03-20 23:47:35 -0500

    add react scan (#3)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
